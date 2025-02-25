d3a0c3c29669b50b64c7a964f31999b2284488e4
DisnaKing <guillem.rodriguez.eljust@gmail.com>
2025-02-25 12:12:16 +0100

    Fork

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,33 +1,12 @@
-public class Entrenador {
-    private String nom;
-    private String cognom;
-    private String nickname;
-    private int edat;
+public class Entrenador extends Participant{
 
     public Entrenador(String nom, String cognom, String nickname, int edat) {
-        this.nom = nom;
-        this.cognom = cognom;
-        this.nickname = nickname;
-        this.edat = edat;
+        super(nom, cognom, nickname, edat);
     }
 
     public String getCompletePlayerName() {
         return this.nom + " \"" + this.nickname + "\" " + this.cognom;
     }
 
-    public String getNom() {
-        return nom;
-    }
 
-    public String getCognom() {
-        return cognom;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public int getEdat() {
-        return edat;
-    }
 }
---
+++ src/Participant.java
@@ -0,0 +1,28 @@
+public class Participant {
+    protected String nom;
+    protected String cognom;
+    protected String nickname;
+    protected int edat;
+
+    public Participant(String nom, String cognom, String nickname, int edat) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.nickname = nickname;
+        this.edat = edat;
+    }
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getEdat() {
+        return edat;
+    }
+}
