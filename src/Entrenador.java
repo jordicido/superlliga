78ef63d87c625213e005699e175ba5c4ad63abea
Andy-Portatil <andperron@alu.edu.gva.es>
2025-02-25 13:00:11 +0100

    Creado participante como superclase

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,33 +1,11 @@
-public class Entrenador {
+public class Entrenador extends Participant{
     private String nom;
     private String cognom;
     private String nickname;
     private int edat;
 
     public Entrenador(String nom, String cognom, String nickname, int edat) {
-        this.nom = nom;
-        this.cognom = cognom;
-        this.nickname = nickname;
-        this.edat = edat;
+        super(nom, cognom, nickname, edat);
     }
 
-    public String getCompletePlayerName() {
-        return this.nom + " \"" + this.nickname + "\" " + this.cognom;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public String getCognom() {
-        return cognom;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public int getEdat() {
-        return edat;
-    }
 }
--- src/Jugador.java
+++ src/Jugador.java
@@ -1,4 +1,4 @@
-public class Jugador {
+public class Jugador extends Participant{
     private String nom;
     private String cognom;
     private String nickname;
@@ -6,35 +6,8 @@ public class Jugador {
     private Rol rol;
 
     public Jugador(String nom, String cognom, String nickname, int edat, Rol rol) {
-        this.nom = nom;
-        this.cognom = cognom;
-        this.nickname = nickname;
-        this.edat = edat;
+        super(nom, cognom, nickname, edat);
         this.rol = rol;
-    }
-
-    public String getCompletePlayerName() {
-        return this.nom + " \"" + this.nickname + "\" " + this.cognom;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public String getCognom() {
-        return cognom;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public int getEdat() {
-        return edat;
     }
 
     public Rol getRol() {
