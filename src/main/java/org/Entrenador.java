c60e3249a360b1a2627161845f8e63268f36f53e
jordicido <jordi.cidoncha4@gmail.com>
2025-03-13 09:32:08 +0100

    Arreglant els lios de l'equip de carrega

--- src/main/java/org/Entrenador.java
+++ src/main/java/org/Entrenador.java
@@ -5,10 +5,6 @@ public class Entrenador extends Participant{
     public Entrenador(String nom, String cognom, String nickname, int edat) {
         super(nom, cognom, nickname, edat);
     }
-
-    public String getCompletePlayerName() {
-        return this.nom + " \"" + this.nickname + "\" " + this.cognom;
-    }
-
+    
 
 }
