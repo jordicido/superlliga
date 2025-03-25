d56d112c3e7d2990a9ba08955eeecef1f35f1355
jordicido <jordi.cidoncha@gmail.com>
2025-03-25 13:38:27 +0100

    Arreglant empastres de XatGPT

--- src/main/java/org/carregarDades.java
+++ src/main/java/org/carregarDades.java
@@ -13,39 +13,6 @@ public class carregarDades {
             Root root = gson.fromJson(reader, Root.class);
             List<Equip> equips = root.getEquip();
 
-            // Acceder a los equipos
-            for (Equip equip : equips) {
-                System.out.println("Equipo: " + equip.getNom());
-
-                // Acceder a la lista de jugadores titulares
-                if (equip.titulars != null) {
-                    System.out.println("Jugadores titulares:");
-                    for (Jugador jugador : equip.getTitulars()) {
-                        System.out.println(" - " + jugador.getNickname() + " (" + jugador.getRol() + ")");
-                    }
-                }
-
-                // Acceder a la lista de suplentes
-                if (equip.reserves != null) {
-                    System.out.println("Suplentes:");
-                    for (Jugador suplente : equip.getReserves()) {
-                        System.out.println(" - " + suplente.getNickname() + " (" + suplente.getRol() + ")");
-                    }
-                }
-
-                System.out.println("------------------");
-            }
-
-            for (Equip equip : root.getEquip()) {
-                if (equip.getTitulars() == null) {
-                    System.out.println("⚠ La lista de titulares de " + equip.getNom() + " es NULL.");
-                } else if (equip.getTitulars().isEmpty()) {
-                    System.out.println("⚠ La lista de titulares de " + equip.getNom() + " está vacía.");
-                } else {
-                    System.out.println("✔ Jugadores cargados correctamente.");
-                }
-            }
-
             return equips;
 
         } catch (Exception e) {
